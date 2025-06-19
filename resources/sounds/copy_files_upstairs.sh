fe809f455feb1fed649a377be3758b3c5c95e13e
Mark-MDO47 <mdo@ieee.org>
2025-06-19 16:39:17 -0700

    Various flavors of voltage levels

---
+++ resources/sounds/copy_files_upstairs.sh
@@ -0,0 +1,6 @@
+cp ./0001_yx5200_chirp.wav D:/001.wav
+cp ./0002_yx5200_mrfossy__sfx_stickerripper_foilbooms_01.wav D:/002.wav
+cp ./0003_yx5200_silence.wav D:/003.wav
+ 
+mkdir D:/ATTRIBUTIONS
+cp  ./Attributions.html D:/ATTRIBUTIONS
---
+++ resources/sounds/run_copyem_upstairs.sh
@@ -0,0 +1,2 @@
+source ~/bashrc.mdo
+python /c/Users/mdo/Documents/GitHub_Mark-MDO47/AudioPlayer-YX5200/copyem.py -d . -s D: -f 0003_YX5200_Silence.wav -l > copy_files_upstairs.sh
